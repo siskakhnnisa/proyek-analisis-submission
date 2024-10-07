6625fc532a17bf26912908639defa9b9858631f7
siskakhnnisa <siskakhnnisa@gmail.com>
2024-10-07 18:51:12 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -168,6 +168,26 @@ with tab2:
     plt.ylabel("Count")
     plt.xticks(fontsize=12)
     st.pyplot(fig)
+
+with tab3:
+    st.subheader("Customer Geolocation Map")
+    customers_silver = pd.read_csv("./data/geolocation_dataset.csv")  
+
+    def plot_brazil_map_streamlit(data): 
+        url = 'https://i.pinimg.com/originals/3a/0c/e1/3a0ce18b3c842748c255bc0aa445ad41.jpg'
+        brazil = mpimg.imread(urllib.request.urlopen(url), 'jpg')
+        
+        fig, ax = plt.subplots(figsize=(10, 10))
+        data.plot(kind="scatter", x="geolocation_lng", y="geolocation_lat", 
+                  alpha=0.3, s=0.3, c='maroon', ax=ax)
+        
+        ax.imshow(brazil, extent=[-73.98283055, -33.8, -33.75116944, 5.4])
+        plt.axis('off')  
+        
+        st.pyplot(fig)
+
+    if st.button('Show Customer Geolocation Map'): 
+        plot_brazil_map_streamlit(customers_silver.drop_duplicates(subset='customer_unique_id'))
     
 # Customer Spend Money
 st.subheader("Customer Spend Money")
@@ -227,25 +247,4 @@ ax[1].tick_params(axis='y', labelsize=35)
 ax[1].tick_params(axis='x', labelsize=30)
 
 st.pyplot(fig)
-
-with tab3:
-    st.subheader("Customer Geolocation Map")
-    customers_silver = pd.read_csv("./dashboard/geolocation_dataset.csv")  
-
-    def plot_brazil_map_streamlit(data): 
-        url = 'https://i.pinimg.com/originals/3a/0c/e1/3a0ce18b3c842748c255bc0aa445ad41.jpg'
-        brazil = mpimg.imread(urllib.request.urlopen(url), 'jpg')
-        
-        fig, ax = plt.subplots(figsize=(10, 10))
-        data.plot(kind="scatter", x="geolocation_lng", y="geolocation_lat", 
-                  alpha=0.3, s=0.3, c='maroon', ax=ax)
-        
-        ax.imshow(brazil, extent=[-73.98283055, -33.8, -33.75116944, 5.4])
-        plt.axis('off')  
-        
-        st.pyplot(fig)
-
-    if st.button('Show Customer Geolocation Map'): 
-        plot_brazil_map_streamlit(customers_silver.drop_duplicates(subset='customer_unique_id'))  
-
 st.caption('Copyright (C) Siska Khoirunnisa (2024)')
