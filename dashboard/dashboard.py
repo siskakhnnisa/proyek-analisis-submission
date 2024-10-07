9864b655e1fde095a9b231d353a5c6e76e7d409f
siskakhnnisa <siskakhnnisa@gmail.com>
2024-10-07 16:52:52 +0700

    Update dashboard.py
    Add more library

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 import matplotlib.image as mpimg 
+import urllib
 from babel.numbers import format_currency
 sns.set(style='dark')
 
